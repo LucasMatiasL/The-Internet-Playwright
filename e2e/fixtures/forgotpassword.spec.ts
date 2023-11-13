1a23538829680ec35557d1f17001279ec1b5389a
Lucas Matias Lepe <es.lucas95@gmail.com>
2023-11-13 17:51:23 -0300

    Created Forgot Password page and test

---
+++ e2e/fixtures/forgotpassword.spec.ts
@@ -0,0 +1,21 @@
+import { test, Page, expect } from '@playwright/test';
+import { ForgotPassword } from '../pages/ForgotPassword';
+
+test.describe('Forgot Password tests', () => {
+    let frgtPassword: ForgotPassword;
+
+    test.beforeEach(async ({ page }) => {
+        frgtPassword = new ForgotPassword(page);
+        await page.goto("https://the-internet.herokuapp.com/forgot_password");
+    });
+
+    test('Write email and click the retrieve button', async ({ page }) => {
+        test.info().annotations.push({
+            type: 'Info',
+            description: "Currently this feature doesn't work"
+        });
+        await frgtPassword.insertMailAndClickBtn();
+    });
+    
+    
+})
---
+++ e2e/pages/ForgotPassword.ts
@@ -0,0 +1,22 @@
+import { type Page, type Locator } from '@playwright/test';
+
+export class ForgotPassword{
+    readonly page: Page;
+    readonly textBox: Locator;
+    readonly retrievePassBtn: Locator;
+
+
+    constructor(page: Page){
+        this.page = page;
+        this.textBox = page.getByLabel("E-mail");
+        this.retrievePassBtn = page.getByRole("button", {
+          name: "Retrieve password",
+        });
+    }
+
+
+    async insertMailAndClickBtn(){
+        await this.textBox.fill("playwrightTest@gmail.com");
+        await this.retrievePassBtn.click();
+    }
+}
