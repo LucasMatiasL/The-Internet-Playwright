d320f2f525a2543121de2ce4683e38d5e02f131a
Lucas Matias Lepe <es.lucas95@gmail.com>
2023-11-14 23:03:02 -0300

    Created Inputs page and tests

---
+++ e2e/fixtures/upload/inputs.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from "@playwright/test";
+import { Inputs } from "../../pages/Inputs";
+
+test.describe("Inputs tests", () => {
+  let input: Inputs;
+
+  test.beforeEach(async ({ page }) => {
+    input = new Inputs(page);
+    await page.goto("https://the-internet.herokuapp.com/inputs");
+  });
+
+  test("Text Box is visible", async ({ page }) => {
+    await expect(input.textBox).toBeVisible();
+  });
+
+  test("Text Box is empty", async ({ page }) => {
+    await expect(input.textBox).toBeEmpty();
+  });
+
+  test("Text Box is editable", async ({ page }) => {
+    await expect(input.textBox).toBeEditable();
+  });
+
+  test("Write numbers in Text Box", async ({ page }) => {
+    await input.writeNumbers("12345");
+    await expect(input.textBox).toHaveValue("12345");
+  });
+
+  test("Try to write numbers in the Text Box", async ({ page }) => {
+    await input.tryToWriteText("This shouldn't allow text, only numbers.");
+    await expect(input.textBox).toBeEmpty();
+  });
+  
+});
---
+++ e2e/pages/Inputs.ts
@@ -0,0 +1,26 @@
+import { type Page, type Locator } from "@playwright/test";
+
+export class Inputs {
+  readonly page: Page;
+  readonly textBox: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.textBox = page.getByRole("spinbutton");
+  }
+
+  async writeNumbers(numbers: string) {
+    await this.textBox.fill(numbers);
+  }
+
+  async tryToWriteText(text: string){
+    try {
+        await this.textBox.fill(text)
+    } catch (error) {
+        console.log("Can't write text here.")
+    }
+  }
+
+
+
+}
