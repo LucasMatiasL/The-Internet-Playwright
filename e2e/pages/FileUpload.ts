ca77ef0a3505b0a493ed41e14f9a9ce33d1db89f
Lucas Matias Lepe <es.lucas95@gmail.com>
2023-11-17 21:14:08 -0300

    test

--- e2e/pages/FileUpload.ts
+++ e2e/pages/FileUpload.ts
@@ -1,23 +1,27 @@
 import { type Locator, type Page } from "@playwright/test";
 import path from "path";
 
-export class FileUpload{
-    readonly page: Page;
-    readonly chooseFileBtn: Locator;
-    readonly uploadFileBtn: Locator;
-    readonly fileUploadedMsg: Locator;
-    readonly nameOfFile: Locator;
+export class FileUpload {
+  readonly page: Page;
+  readonly chooseFileBtn: Locator;
+  readonly uploadFileBtn: Locator;
+  readonly fileUploadedMsg: Locator;
+  readonly nameOfFile: Locator;
 
-    constructor(page: Page){
-        this.page = page;
-        this.chooseFileBtn = page.locator("#file-upload");
-        this.uploadFileBtn = page.getByRole("button", { name: "Upload" });
-        this.fileUploadedMsg = page.getByRole("heading", { name: "File Uploaded!" });
-        this.nameOfFile = page.getByText("upload.txt");
-    }
+  constructor(page: Page) {
+    this.page = page;
+    this.chooseFileBtn = page.locator("#file-upload");
+    this.uploadFileBtn = page.getByRole("button", { name: "Upload" });
+    this.fileUploadedMsg = page.getByRole("heading", {
+      name: "File Uploaded!",
+    });
+    this.nameOfFile = page.getByText("upload.txt");
+  }
 
-    async uploadFile(){
-        await this.chooseFileBtn.setInputFiles(path.join('e2e\\fixtures\\upload\\', 'upload.txt'));
-        await this.uploadFileBtn.click();
-    }
+  async uploadFile() {
+    await this.chooseFileBtn.setInputFiles(
+      path.join("e2e\\fixtures\\upload", "upload.txt")
+    );
+    await this.uploadFileBtn.click();
+  }
 }
